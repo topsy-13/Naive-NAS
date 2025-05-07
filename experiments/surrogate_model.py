ad796db01cbae9c334b37286461ca6cedad62d52
topsy-13 <santiramir98@gmail.com>
2025-05-06 20:10:46 -0500

    model created

---
+++ experiments/surrogate_model.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+import naiveautoml
+
+raw_df = pd.read_csv('experiments/lc_bench_results/all_datasets_lw_clean.csv')
+dataset_names = raw_df['dataset_name'].unique()
+
+for dataset_name in dataset_names:
+    print(f"Processing dataset: {dataset_name}")
+    clean_df = raw_df[raw_df['dataset_name'] == dataset_name].copy()
+    model_df = clean_df.drop(columns='dataset_name')
+
+    target = 'final_val_balanced_accuracy'
+    X = model_df.drop(target, axis=1)
+    y = model_df[target]
+
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=13)
+
+    naml = naiveautoml.NaiveAutoML(timeout_overall=60)
+    naml.fit(X_train, y_train)
+
+    pipeline = naml.chosen_model
+    # log the pipeline
+    with open(f'experiments/lc_bench_results/predicted_ebe_performance/EBE_NAS_pipeline.txt', 'a') as f:
+        f.write(dataset_name)
+        f.write(f'{str(pipeline)}\n')
+
+    pipeline.fit(X_train, y_train)
+
+    y_pred = pipeline.predict(X_val)
+
+    mse = mean_squared_error(y_val, y_pred)
+    r2 = r2_score(y_val, y_pred)
+
+    print(f"Validation MSE: {mse:.4f}")
+    print(f"Validation R²: {r2:.4f}")
+
+    new_archs_df = pd.read_csv('experiments/lc_bench_results/EBE_NAS_all_results_features.csv')
+    new_archs_df = new_archs_df[new_archs_df['dataset_name'] == dataset_name]
+    new_archs_features = new_archs_df[X_train.columns]
+
+    predicted_performance = pipeline.predict(new_archs_features)
+    new_archs_df['predicted_balanced_accuracy'] = predicted_performance
+
+    new_archs_df.to_csv(f'experiments/lc_bench_results/predicted_ebe_performance/EBE_NAS_predicted_{dataset_name}.csv', index=False)
